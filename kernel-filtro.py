d9b9805b4fe0d42362f0f53f73dc646b23a92c81
luisaxsz <marialuisalameirao@gmail.com>
2024-11-17 14:04:41 -0300

    feat: filtros

--- kernel-filtro.py
+++ kernel-filtro.py
@@ -14,13 +14,12 @@ import cv2
 import numpy as np
 import sys
 
-from mascaras import background_substractor
-
 VIDEO = "Dados/Ponte.mp4";
 
 algoritmo_types = ['KNN', 'GMG', 'CNT', 'MOG', 'MOG2'];
 algoritmoSelecionado = algoritmo_types[4];
 
+# Tipos de filtragens
 def Kernel(KERNEL_TYPE):
     match KERNEL_TYPE:
         case 'dilation':
@@ -32,13 +31,25 @@ def Kernel(KERNEL_TYPE):
         case _:
             return print("Kernel não encontrado"), sys.exit(1);
     return kernel
-print("Dilation: ")
-print(Kernel('dilation'))
-print("Opening: ")
-print(Kernel('opening'))
-print("Closing: ")
-print(Kernel('closing'))
 
+# Adicionar filtragem aos videos usando openCV
+def Filter(img, filter):
+    match filter:
+        case 'dilation':
+            return cv2.dilate(img, Kernel('dilation'), iterations=2)
+        case 'opening':
+            return cv2.morphologyEx(img, cv2.MORPH_OPEN, Kernel('opening'), iterations=2)
+        case 'closing':
+            return cv2.morphologyEx(img, cv2.MORPH_CLOSE, Kernel('closing'), iterations=2) # Faz filtro close duas vezes
+        case 'combine':
+            closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, Kernel('closing'), iterations=2)
+            opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, Kernel('opening'), iterations=2)
+            dilation = cv2.dilate(opening, Kernel('dilation'), iterations=2)
+            return dilation # Custo maior porém maior qualidade
+        case _:
+            return print("Filtro não foi feito"), sys.exit(1)
+
+# Adiciona a mascara ao frame
 def Substractor(algoritmoSelecionado):
     match algoritmoSelecionado:
         case 'KNN':
@@ -57,6 +68,7 @@ def Substractor(algoritmoSelecionado):
 capture = cv2.VideoCapture(VIDEO)
 background_substractor = Substractor(algoritmoSelecionado)
 
+# Função principal - Aplica as mascaras e os filtros
 def main():
     while (capture.isOpened()):
         hasFrame, frame = capture.read();
@@ -65,14 +77,17 @@ def main():
             print("Fim dos Frames");
             break;
 
-        frame = cv2.resize(frame, (0, 0), fx=0.35, fy=0.35)
+        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
         mask = background_substractor.apply(frame)
+        # Escolher filtro para aplicar na mascara
+        mask_Filter  = Filter(mask, 'combine')
+        # Aplicar mascara ao frame
+        cars_after_mask = cv2.bitwise_and(frame, frame, mask=mask_Filter)
+
+        cv2.imshow('Mask', cars_after_mask)
 
         if cv2.waitKey(1) & 0xFF == ord('c'):
             break;
-
-        cv2.imshow('Frame', frame)
-        cv2.imshow('Mask', mask)
-
 
 main();
